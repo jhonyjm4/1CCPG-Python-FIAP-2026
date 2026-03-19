3188f80e3bed6a143e672b8d2e317f91224bc6ee
jhonyjm4 <jhony.melo.silva@gmail.com>
2026-03-19 11:42:28 -0300

    aula 02 variaveis

---
+++ aula02-Variavéis-Operadores/aula02a-variaveis.py
@@ -0,0 +1,14 @@
+print('hello world')
+
+print(7 + 4)
+print("7 + 4")
+print('7' + '4')
+
+numero = float(input('Digite um numero: '))
+
+
+if numero >= 8:
+    print('maior ou igual a 8')
+else:
+    print('menor que 8')
+
